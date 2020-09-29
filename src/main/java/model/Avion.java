bcf7ccd56501db0f671fd04df0d2a6b4f8024f17
MilosRadin <milosradin86@gmail.com>
2020-09-30 00:15:52 +0200

    Uradjena klasa avion closes #5

---
+++ src/main/java/model/Avion.java
@@ -0,0 +1,65 @@
+package model;
+
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName="avion")
+public class Avion {
+
+    public static final String POLJE_OZNAKA="oznaka";
+    public static final String POLJE_RASPON_KRILA="raspon_krila";
+
+    @DatabaseField(generatedId = true)
+    public int id;
+    @DatabaseField(columnName = "POLJE_OZNAKA", canBeNull = false)
+    public String oznaka;
+    @DatabaseField(columnName = POLJE_RASPON_KRILA, canBeNull = false)
+    public int raspon_krila;
+
+    @ForeignCollectionField(foreignFieldName = "artikal")
+    private ForeignCollection<Roba> robe;
+
+
+    public Avion() {
+    }
+
+    public Avion(String oznaka, int raspon_krila) {
+        this.oznaka = oznaka;
+        this.raspon_krila = raspon_krila;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getOznaka() {
+        return oznaka;
+    }
+
+    public void setOznaka(String oznaka) {
+        this.oznaka = oznaka;
+    }
+
+    public int getRaspon_krila() {
+        return raspon_krila;
+    }
+
+    public void setRaspon_krila(int raspon_krila) {
+        this.raspon_krila = raspon_krila;
+    }
+
+    @Override
+    public String toString() {
+        return "Avion{" +
+                "id=" + id +
+                ", oznaka='" + oznaka + '\'' +
+                ", raspon_krila=" + raspon_krila +
+                '}';
+    }
+}
